6189dae86e798b7bf42732b3a2d7faf60a8a4c3f
Allison Aisha <aishaallison007@gmail.com>
2026-05-15 19:20:15 +0000

    frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import DealrApp from "./dealr-app.tsx"
+import DealrApp from "./newDealr-app.tsx"
 
 createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
